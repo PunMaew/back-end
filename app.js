a1d3b92f5dd6c8e2b6acb4da6ea7062698fb34a1
CL <60165701+chananananu@users.noreply.github.com>
2022-05-05 13:09:36 +0700

    init

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const PORT = 3000;
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connection Success.");
+  })
+  .catch((err) => {
+    console.error("Mongo Connection Error", err);
+  });
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); //optional
+app.get("/ping", (req, res) => {
+  return res.send({
+    error: false,
+    message: "Server is healthy",
+  });
+});
+app.use('/user', require('./routes/users'));
+app.listen(PORT, () => {
+  console.log("Server started listening on PORT : " + PORT);
+});
