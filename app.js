6ad67f9ae308a1c4b1f0d63d67840181b16370cd
Piyathida Thaneewan <piyathida.thaneewan@mail.kmutt.ac.th>
2022-05-27 01:58:29 +0700

    edit app.js 23

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGO_URI, {
   });
 const app = express();
 const corsOptions = {
-  origin: 'http://localhost:8080',
+  origin: 'http://punmaew.sit.kmutt.ac.th:8080',
   credentials: true,
 };
 app.use(bodyParser.json());
